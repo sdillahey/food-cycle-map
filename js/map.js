6fcf138b8912ae4caa3692a9a90d07cadd6fad4d
Stacy Dillahey <stacydillahey@Stacys-Air.lan>
2020-12-02 11:49:17 -0800

    Add location info to markers

--- js/map.js
+++ js/map.js
@@ -10,37 +10,89 @@ function searchThisArea(lat, long) {
 
 // GOOGLE MAP
 let map;
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 34.0354899, lng: -118.2439235 },
-    zoom: 11,
-  });
+async function initMap() {
+	var map = new google.maps.Map(document.getElementById("map"), {
+		center: { lat: 34.0354899, lng: -118.2439235 },
+	    zoom: 11,
+	});
+	var marker;
+	let places = await getPlaces();
+	places.forEach(function(place) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(getLatitude(place), getLongitude(place)),
+			map: map,
+			title: getName(place)
+		});
+		var contentString = buildLocationInfo(place)
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString,
+		});
+		marker.addListener("click", () => {
+			infowindow.open(map, marker);
+		});
+	});
 }
 
-google.maps.event.addListener(map, "center_changed", function() {
-  var center = this.getCenter();
-  var latitude = center.lat();
-  var longitude = center.lng();
-});
+function mapChange() {
+	google.maps.event.addListener(map, "center_changed", function() {
+	  var center = this.getCenter();
+	  var latitude = center.lat();
+	  var longitude = center.lng();
+	});
+}
 
 
 // FOOD OASIS
 // Public Methods
-function getPlaces(lat=null, long=null) {
-	return fetchFoodOasis(lat, long)
+async function getPlaces(lat, long) {
+	try {
+		return await fetchFoodOasis(lat, long);
+	} catch(err) {
+		console.log(`Error fetching from FoodOasis: $(err)`);
+	}
 }
 
 // FoodOasis API
 // GET FoodOasis Response Data
 async function fetchFoodOasis(latitude=34.035, longitude=-118.244) {
-  let response = await fetch(`https://foodoasis.la/api/stakeholderbests?categoryIds[]=1&categoryIds[]=9&latitude=${latitude}&longitude=${longitude}&distance=4&isInactive=false&verificationStatusId=0&tenantId=1`);
+  let response = await fetch(`https://foodoasis.la/api/stakeholderbests?categoryIds[]=1&categoryIds[]=9&latitude=${latitude}&longitude=${longitude}&distance=6&isInactive=false&verificationStatusId=0&tenantId=1`);
   return await response.json();
 }
 
 // Helper Functions
+function buildLocationInfo(place) {
+	var mailing_address = buildMailingAddress(place);
+	var info = '<div id="locationBodyContent">' +
+		`<p><b><a href=${getWebsite(place)}>${getName(place)}</a></b></p>` +
+		`<p>${getPhone(place)}</p>` +
+		`<p>${mailing_address[0]}</p>` +
+		`<p>${mailing_address[1]}</p>`
+	return info
+}
+
 function buildMailingAddress(place) {
 	let mailing_address = [];
 	mailing_address.push(place.address2 ? `${place.address1} ${place.address2}` : `${place.address1}`)
 	mailing_address.push(`${place.city}, ${place.state} ${place.zip}`)
 	return mailing_address
+}
+
+function getName(place) {
+	return place['name']
+}
+
+function getPhone(place) {
+	return place['phone']
+}
+
+function getWebsite(place) {
+	return place['website']
+}
+
+function getLatitude(place) {
+	return place['latitude']
+}
+
+function getLongitude(place) {
+	return place['longitude']
 }
